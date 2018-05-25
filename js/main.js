0897e8d4f51c056d390b28e005870269016af2d4
CharlesJxx <jxx0000_8@hotmail.com>
2018-05-25 10:44:40 +0800

    Optimized for mobile device adaptation

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-function changemenu(){
-    var isDisplay = document.getElementById("adaptive-navigation-bar-list");
-    if(isDisplay.style.display='block'){
-        isDisplay.style.display='none';
-    }
-    if(isDisplay.style.display='none'){
-        isDisplay.style.display='block';
-    }
-}
+$("#adaptive-navigation-bar-button").click(function () {
+    $("#adaptive-navigation-bar-list").slideToggle(100);
+});
